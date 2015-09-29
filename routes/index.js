b5a0afefb3628a76ab241aa3bd4ff6b82095b09a
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-09-29 15:57:30 -0600

    Initial parser commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var esprima = require('esprima');
+var util = require('../utility.js');
+var OutputNode =  require('../outputnode.js');
+var parser = require('../parser.js');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,69 +13,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/api/context', function(req, res) {
-  console.log('request is', req.body);
-  res.writeHead(200);
+  console.log('request is', req.body.codeString);
+  // res.writeHead(200);
   
-  // TODO: get actual response obj
-  var testObj = JSON.stringify({ __contextName__: 'global',
- __localVariables__: [ { hero: 'aHero()' }, { newSaga: '{f}' } ],
- __innerScopes__: 
-  [ { __contextName__: 'newSaga',
-      __localVariables__: [ { foil: 'aFoil()' }, { saga: '{f}' } ],
-      __innerScopes__: 
-       [ { __contextName__: 'saga',
-           __localVariables__: [ { deed: 'aDeed()' } ],
-           __innerScopes__: [ null ] },
-         { __contextName__: 'saga',
-           __localVariables__: [ { deed: 'aDeed()' } ],
-           __innerScopes__: [ null ] } ] },
-    { __contextName__: 'newSaga',
-      __localVariables__: [ { foil: 'aFoil()' }, { saga: '{f}' } ],
-      __innerScopes__: 
-       [ { __contextName__: 'saga',
-           __localVariables__: [ { deed: 'aDeed()' } ],
-           __innerScopes__: [ null ] },
-         { __contextName__: 'saga',
-           __localVariables__: [ { deed: 'aDeed()' } ],
-           __innerScopes__: [ null ] } ] } ] });  
 
-  // var jsonObj = JSON.stringify({
-  // '__contextName__': 'global',
-  // 'hero'      :   'Gal',
-  // 'newSaga'   :   '{f}',
-  // '__innerScopes__' :     [
-  // { 
-  //   '__contextName__': 'newSaga',
-  //   'foil': 'Cow',
-  //   'saga': '{f}',
-  //   '__innerScopes__': [
-  //     {
-  //       '__contextName__': 'saga',
-  //       'deed': "Eyes"
-  //     },
-  //     {
-  //       '__contextName__': 'saga',
-  //       'deed': 'Tips'
-  //     }
-  //   ]
-  // },
-  // {
-  //   '__contextName__': 'newSaga',
-  //   'foil': 'Cat',
-  //   'sagq': '{f}',
-  //   '__innerScopes__': [
-  //     {
-  //       '__contextName__': 'saga',
-  //       'deed': 'Rubs'
-  //     },
-  //     {
-  //       '__contextName__': 'saga',
-  //       'deed': 'Robs'
-  //     }
-  //   ]
-  // }
-  // ]
-  // });
+  var rootNode = esprima.parse(req.body.codeString);
+  // console.log(rootNode);
+  var nodeMap = util.findAllFunctionNodes(rootNode);
+  //console.log(nodeMap);
+  var outputObject = new OutputNode();
+  console.log(outputObject);
+  //console.log(deepInspect(rootNode));
+  parser.parseASTRecursively(rootNode, nodeMap, outputObject);
+  //console.log(outputObject);
+  // TODO: get actual response obj
+  outputObject.formatOutput();
+  var testObj = JSON.stringify(outputObject);  
+
+
+  console.log(testObj);
   res.end(testObj);
 });
 
---
+++ utility.js
@@ -0,0 +1,38 @@
+var util = require('util');
+var esprima = require('esprima');
+var fs = require('fs');
+
+module.exports.findAllFunctionNodes = function(inputTree) {
+  var functionMapping = {};
+  traverse(inputTree, function(nd) {
+    if (nd.type === 'FunctionDeclaration') {
+      functionMapping[nd.id.name] = nd;
+    }
+    if (nd.type === 'VariableDeclaration') {
+      for (var x = 0; x < nd.declarations.length; x++) {
+        if (nd.declarations[x].init.type === 'FunctionExpression') {
+          functionMapping[nd.declarations[x].id.name] = nd.declarations[x];
+        }
+      }
+    }
+  });
+  return functionMapping;
+};
+
+function traverse(node, func) {
+  func(node);
+  for (var key in node) {
+    if (node.hasOwnProperty(key)) {
+      var child = node[key];
+      if (typeof child === 'object' && child !== null) {
+        if (Array.isArray(child)) {
+          child.forEach(function(node) {
+            traverse(node, func);
+          });
+        } else {
+          traverse(child, func);
+        }
+      }
+    }
+  }
+}
